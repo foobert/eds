ecb8953f91a642f937f07df6cc77a80bb10fdbb5
foobert <0x002a@gmail.com>
2017-04-30 22:08:13 +0200

    Chart bounties

--- src/bounty/components/index.js
+++ src/bounty/components/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 
 export default function Bounty(props) {
     return (
         <div>
             <p>Rewarded: {props.bounty.rewarded}</p>
             <p>Collected: {props.bounty.collected}</p>
+            <BarChart
+                width={600} height={300} data={props.bounty.timeline}
+                margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                <XAxis dataKey="datestamp"/>
+                <YAxis/>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <Tooltip/>
+                <Legend />
+                <Bar dataKey="rewarded" fill="#8884d8" />
+                <Bar dataKey="collected" fill="#82ca9d" />
+            </BarChart>
         </div>
     );
 }
--- src/bounty/reducer.js
+++ src/bounty/reducer.js
@@ -1,16 +1,52 @@
-import { ADD_JOURNAL_ENTRY } from 'file-upload/actions';
+import { ADD_JOURNAL_FILE, ADD_JOURNAL_ENTRY } from 'file-upload/actions';
 
-export default function bounty(state = {rewarded: 0, collected: 0}, action) {
+function filterAndSum(entries, eventType, selector) {
+    return entries
+        .filter(e => e.event === eventType)
+        .map(selector)
+        .reduce((s, x) => s + x, 0);
+}
+
+export default function bounty(state = {rewarded: 0, collected: 0, timeline: []}, action) {
     switch (action.type) {
-        case ADD_JOURNAL_ENTRY:
-            switch (action.payload.entry.event) {
-                case 'Bounty':
-                    return {rewarded: state.rewarded, collected: state.collected + action.payload.entry.TotalReward};
-                case 'RedeemVoucher':
-                    return {rewarded: state.rewarded + action.payload.entry.Amount, collected: state.collected};
-                default:
-                    return state;
+        case ADD_JOURNAL_FILE:
+            const bountyEntries = action.payload.entries
+                .filter(e => e.event === 'Bounty' || e.event === 'RedeemVoucher');
+            if (bountyEntries.length === 0) {
+                return state;
             }
+
+            const rewarded = filterAndSum(bountyEntries, 'Bounty', e => e.TotalReward);
+            const collected = filterAndSum(bountyEntries, 'RedeemVoucher', e => e.Amount);
+
+            console.log('TOTAL REWARDS', rewarded);
+            console.log('TOTAL COLLECTED', collected);
+
+            let timeline = state.timeline.slice();
+            const timestamp = new Date(bountyEntries[0].timestamp);
+            const datestamp = new Date(timestamp.getFullYear(), timestamp.getMonth(), timestamp.getDate());
+            const timelineIndex = timeline.findIndex(day => day.datestamp === datestamp);
+            if (timelineIndex === -1) {
+                timeline.push({
+                    datestamp,
+                    rewarded,
+                    collected,
+                });
+                timeline.sort((a, b) => a.datestamp - b.datestamp);
+            } else {
+                const oldTimelineEntry = timeline[timelineIndex];
+                timeline[timelineIndex] = {
+                    datestamp,
+                    rewarded: oldTimelineEntry.rewarded + rewarded,
+                    collected: oldTimelineEntry.collected + collected,
+                };
+            }
+
+            return {
+                rewarded: state.rewarded + rewarded,
+                collected: state.collected + collected,
+                timeline,
+            };
         default:
             return state;
     }
