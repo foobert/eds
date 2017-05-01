7ba86cc352a34b4af6ee6f06c1361490f3a72442
foobert <0x002a@gmail.com>
2017-05-01 15:41:12 +0200

    Replace recharts with plotly

--- src/bounty/components/index.js
+++ src/bounty/components/index.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 
-export default function Bounty(props) {
-    return (
-        <div>
-            <p>Rewarded: {props.bounty.rewarded}</p>
-            <p>Collected: {props.bounty.collected}</p>
-            <BarChart
-                width={600} height={300} data={props.bounty.timeline}
-                margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-                <XAxis dataKey="datestamp"/>
-                <YAxis/>
-                <CartesianGrid strokeDasharray="3 3"/>
-                <Tooltip/>
-                <Legend />
-                <Bar dataKey="rewarded" fill="#8884d8" />
-                <Bar dataKey="collected" fill="#82ca9d" />
-            </BarChart>
-        </div>
-    );
+export default class Bounty extends React.Component {
+    componentDidMount() {
+        this.updatePlot();
+    }
+
+    componentDidUpdate() {
+        this.updatePlot();
+    }
+
+    updatePlot() {
+        const rewarded = {
+            x: this.props.bounty.timeline.map(data => data.datestamp.toString()),
+            y: this.props.bounty.timeline.map(data => data.rewarded),
+            name: 'Rewarded',
+            type: 'bar',
+        };
+        const collected = {
+            x: this.props.bounty.timeline.map(data => data.datestamp.toString()),
+            y: this.props.bounty.timeline.map(data => data.collected),
+            name: 'Collected',
+            type: 'bar',
+        };
+        const data = [rewarded, collected];
+        Plotly.newPlot('plot', data, {margin: {t: 0 }});
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Rewarded: {this.props.bounty.rewarded}</p>
+                <p>Collected: {this.props.bounty.collected}</p>
+                <div id='plot'></div>
+            </div>
+        );
+    }
 }
